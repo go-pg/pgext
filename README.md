643e4e9690cf5ac9038f218eddbddc02a19861a8
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-09-05 09:58:06 +0300

    Add readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# go-pg extensions
+
+## Faster JSON encoding by segmentio
+
+```go
+import (
+    "github.com/go-pg/pg/v10/pgjson"
+    "github.com/go-pg/pgext"
+)
+
+func init() {
+    pgjson.SetProvider(pgext.SegmentJSONProvider{})
+}
+```
+
+## Tracing using OpenTelemetryHook
+
+For more details see [documentation](https://pg.uptrace.dev/tracing/):
+
+```go
+db := pg.Connect(&pg.Options{...})
+db.AddQueryHook(&pgext.OpenTelemetryHook{})
+```
+
+## Print error and a query using DebugHook
+
+```go
+db := pg.Connect(&pg.Options{...})
+
+if debug {
+    db.AddQueryHook(&pgext.DebugHook{
+        //Verbose: true,
+    })
+}
+```
--- debug.go
+++ debug.go
@@ -2,29 +2,32 @@ package pgext
 
 import (
 	"context"
-	"log"
+	"fmt"
 
 	"github.com/go-pg/pg/v10"
 )
 
-// DebugHook is a query hook that logs the query and the error if there are any.
+// DebugHook is a query hook that logs an error with a query if there are any.
 // It can be installed with:
 //
 //   db.AddQueryHook(pgext.DebugHook{})
-type DebugHook struct{}
+type DebugHook struct {
+	// Verbose causes hook to print all queries (even those without an error).
+	Verbose bool
+}
 
 var _ pg.QueryHook = (*DebugHook)(nil)
 
-func (DebugHook) BeforeQuery(ctx context.Context, evt *pg.QueryEvent) (context.Context, error) {
+func (h DebugHook) BeforeQuery(ctx context.Context, evt *pg.QueryEvent) (context.Context, error) {
 	q, err := evt.FormattedQuery()
 	if err != nil {
 		return nil, err
 	}
 
 	if evt.Err != nil {
-		log.Printf("Error %s executing query:\n%s\n", evt.Err, q)
-	} else {
-		log.Printf("%s", q)
+		fmt.Printf("%s executing a query:\n%s\n", evt.Err, q)
+	} else if h.Verbose {
+		fmt.Println(string(q))
 	}
 
 	return ctx, nil
